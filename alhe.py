51efbe55968090dba810c62a03fac2fabb76cc56
Wikront <wiktorgrzesiuk@gmail.com>
2017-06-10 16:53:14 +0200

    list of cars sorted

--- alhe.py
+++ alhe.py
@@ -11,6 +11,7 @@ pasy:
 '''
 import random
 from datetime import time
+import math
 
 FILE = open('data.txt', 'w')
 HOURS = [7, 8, 8, 9, 15, 16, 16, 17, 18]
@@ -27,10 +28,21 @@ class car():
         return str(self.time.strftime("%H:%M:%S ") + str(self.direction_from) + ' ' + str(self.direction_to) + ' ' + str(self.lane))
 
 def generate_one_car_data():
-    hour = random.randrange(0, 24, 1)
+    hour = random_hour_generator(HOURS).get_random_hour()
     direction_from = random.randrange(0, 4, 1)
-    direction_to = random.randrange(0, 4, 1)
-    lane = random.randrange(0, 3, 1)
+    if(hour > 7 and hour < 10):
+        direction_to = random_direction_generator(DIRECTIONS_MORNING, direction_from).get_random_direction()
+    elif(hour > 14 and hour < 19):
+        direction_to = random_direction_generator(DIRECTIONS_AFTERNOON, direction_from).get_random_direction()
+    else:
+        direction_to = random_direction_generator([], direction_from).get_random_direction()
+
+    if math.fabs(direction_from - direction_to)%4 == 2:
+        lane = random.randrange(1, 3, 1)
+    elif math.fabs(direction_from - direction_to)%4 == 1:
+        lane = 2
+    else:
+        lane = 0
     minute = random.randrange(0, 60, 1)
     second = random.randrange(0, 60, 1)
     return car(hour, minute, second, direction_from, direction_to, lane)
@@ -39,20 +51,30 @@ class random_hour_generator():
     def __init__(self, list):
         self.hours = [i for i in range(24)] + list
     def get_random_hour(self):
-        random.choice(self.hours)
+        return random.choice(self.hours)
     def print_hours(self):
         print(self.hours)
 
 class random_direction_generator():
-    def __init__(self, list):
+    def __init__(self, list, from_direction):
         self.directions = [0, 1, 2, 3] + list
-    def get_random_hour(self):
-        random.choice(self.directions)
+        self.directions.remove(from_direction)
+    def get_random_direction(self):
+        return random.choice(self.directions)
+
+class cars_set():
+    def __init__(self):
+        self.list = []
+    def generate_data(self, count):
+        for i in range(count):
+            self.list.append(generate_one_car_data())
+        self.list.sort(key = lambda x: x.time)
+        return self.list
 
 def main():
+    cars = cars_set().generate_data(300)
     for i in range(300):
-        FILE.write(generate_one_car_data().to_string() + '\n')
-    random_hour_generator([2,3,4,5]).print_hours()
+        FILE.write(cars[i].to_string() + '\n')
 	
 main()
 
