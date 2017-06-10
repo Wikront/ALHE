8b350893bc660ab9c242545b7316e972bf4559c0
Wikront <wiktorgrzesiuk@gmail.com>
2017-06-10 15:58:51 +0200

    tab from tostring removed

--- alhe.py
+++ alhe.py
@@ -24,14 +24,13 @@ class car():
         self.direction_to = direction_to
         self.lane = lane
     def to_string(self):
-        return str(self.time.strftime("%H:%M:%S	") + ' ' + 
-                   str(self.direction_from) + ' ' + str(self.direction_to) + ' ' + str(self.lane))
+        return str(self.time.strftime("%H:%M:%S ") + str(self.direction_from) + ' ' + str(self.direction_to) + ' ' + str(self.lane))
 
 def generate_one_car_data():
+    hour = random.randrange(0, 24, 1)
     direction_from = random.randrange(0, 4, 1)
     direction_to = random.randrange(0, 4, 1)
     lane = random.randrange(0, 3, 1)
-    hour = random.randrange(0, 24, 1)
     minute = random.randrange(0, 60, 1)
     second = random.randrange(0, 60, 1)
     return car(hour, minute, second, direction_from, direction_to, lane)
